ae4086b8a5c84bdf1b75dbf162453e55beb28982
Chris Brandhorst <chris.brandhorst@topicus.nl>
2018-12-08 15:03:23 +0100

    Some sugaring

--- 2018/08/main.rb
+++ 2018/08/main.rb
@@ -31,22 +31,22 @@ end
 
 data = File.read("input").split(' ').map(&:to_i)
 
-def get_node(i, data)
+def get_node(pointer, data)
   node = Node.new
 
-  child_count = data[i]; i += 1
-  meta_count = data[i]; i += 1
+  child_count, meta_count = data[pointer, 2]
+  pointer += 2
 
   child_count.times do |c|
-    child_node, i = get_node(i, data)
+    child_node, pointer = get_node(pointer, data)
     node << child_node
   end
 
-  node.meta = data[i...i + meta_count]
-  [node, i + meta_count]
+  node.meta = data[pointer, meta_count]
+  [node, pointer + meta_count]
 end
 
-root, i = get_node(0, data)
+root = get_node(0, data).first
 part1 = root.branch.map(&:meta).flatten.sum
 puts "Part 1: #{part1}"
 puts "Part 2: #{root.value}"
