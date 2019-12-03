4ec74f74df90fb278df5ad941f9d2cbbd56f2cb9
Chris Brandhorst <chris.brandhorst@topicus.nl>
2019-12-03 07:50:04 +0100

    Rest of 2018

---
+++ 2018/19/main.rb
@@ -0,0 +1,98 @@
+OP_KEYS = %w{addr addi mulr muli banr bani borr bori setr seti gtir gtri gtrr eqir eqri eqrr}
+
+class Op
+
+  attr_reader :opkey
+
+  def initialize(op)
+    @opkey, @ina, @inb, @out = op
+  end
+
+  def run!(register)
+    send("#{@opkey}!",register)
+  end
+
+  def addr(register); register[@ina] + register[@inb]; end
+  def addi(register); register[@ina] + @inb; end
+  def mulr(register); register[@ina] * register[@inb]; end
+  def muli(register); register[@ina] * @inb; end
+  def banr(register); register[@ina] & register[@inb]; end
+  def bani(register); register[@ina] & @inb; end
+  def borr(register); register[@ina] | register[@inb]; end
+  def bori(register); register[@ina] | @inb; end
+  def setr(register); register[@ina]; end
+  def seti(register); @ina; end
+  def gtir(register); @ina > register[@inb] ? 1 : 0; end
+  def gtri(register); register[@ina] > @inb ? 1 : 0; end
+  def gtrr(register); register[@ina] > register[@inb] ? 1 : 0; end
+  def eqir(register); @ina == register[@inb] ? 1 : 0; end
+  def eqri(register); register[@ina] == @inb ? 1 : 0; end
+  def eqrr(register); register[@ina] == register[@inb] ? 1 : 0; end
+
+  OP_KEYS.each do |opkey|
+    define_method("is_#{opkey}?") do |register|
+      register[@out] == send("#{opkey}", register)
+    end
+    define_method("#{opkey}!") do |register|
+      register[@out] = send("#{opkey}", register)
+    end
+  end
+
+end
+
+lines = File.readlines("input")
+ip_reg = lines.shift.match(/#ip (\d+)/).captures.first.to_i
+
+data = lines.map{ |l| l.match(/([a-z]+) (\d+) (\d+) (\d+)/){ |m|
+  Op.new(m.captures.each_with_index.map{ |v,i| i == 0 ? v : v.to_i }) }
+}
+
+def run(data, register, ip_reg, ip)
+  0.step do |i|
+    p ip
+    p register
+    register[ip_reg] = ip
+    op = data[ip]
+    op.run!(register)
+    ip = register[ip_reg] + 1
+    break if data[ip].nil?
+  end
+
+  register[0]
+end
+
+def run2(data, register, ip_reg, ip)
+  0.step do |i|
+
+    if ip == 3
+      puts "------------------"
+      p register
+      if register[5] % register[1] == 0
+        if register[1] <= 1
+          p "A"
+          register[4] = register[5] / register[1]
+          register[3] = register[5]
+        # else
+        #   p "C"
+        #   register[3] = register[5] / register[1]
+        end
+      else
+        register[3] = register[5]
+        p "B"
+      end
+      p register
+    end
+
+    register[ip_reg] = ip
+    op = data[ip]
+    op.run!(register)
+    ip = register[ip_reg] + 1
+
+    break if data[ip].nil?
+  end
+
+  register[0]
+end
+
+p run2(data, [0] * 6, ip_reg, 0)
+# p run2(data, [1, 0, 0, 0, 0, 0], ip_reg, 0)
---
+++ 2018/20/main.rb
@@ -0,0 +1,192 @@
+data = File.read("input")
+# data = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$"
+# data = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$"
+# data = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
+# data = "^ENWWW(NEEE|SSE(EE|N))$"
+
+data = data[1..-2]
+
+class Area
+
+  attr_reader :area, :width, :height
+  def initialize(area)
+    @area = area
+  end
+
+  def extend_n!(x,y)
+    ext_y = 0
+    if at_n_edge?(x, y)
+      empties = " " * @area.first.size
+      @area.unshift(empties, empties.dup)
+      ext_y = 2
+      y += 2
+    end
+
+    existing_room = @area[y - 2][x] == '.'
+
+    @area[y - 2][x - 1] = '?' unless @area[y - 2][x - 1] == '|'
+    @area[y - 2][x] = '.' unless existing_room
+    @area[y - 2][x + 1] = '?' unless @area[y - 2][x + 1] == '|'
+    @area[y - 3][x - 1] = '#'
+    @area[y - 3][x] = '?' unless @area[y - 3][x] == '-'
+    @area[y - 3][x + 1] = '#'
+    
+    @area[y - 1][x] = '-'
+    [x, y - 2, 0, ext_y, existing_room]
+  end
+
+  def extend_e!(x, y)
+    @area.each{ |r|  r.concat("  ") } if at_e_edge?(x, y)
+
+    existing_room = @area[y][x + 2] == '.'
+
+    @area[y - 1][x + 2] = '?' unless @area[y - 1][x + 2] == '-'
+    @area[y][x + 2] = '.' unless existing_room
+    @area[y + 1][x + 2] = '?' unless @area[y + 1][x + 2] == '-'
+    @area[y - 1][x + 3] = '#'
+    @area[y][x + 3] = '?' unless @area[y][x + 3] == '|'
+    @area[y + 1][x + 3] = '#'
+
+    @area[y][x + 1] = '|'
+    [x + 2, y, 0, 0, existing_room]
+  end
+
+  def extend_s!(x,y)
+    if at_s_edge?(x, y)
+      empties = " " * @area.first.size
+      @area.push(empties, empties.dup)
+    end
+
+    existing_room = @area[y + 2][x] == '.'
+
+    @area[y + 2][x - 1] = '?' unless @area[y + 2][x - 1] == '|'
+    @area[y + 2][x] = '.' unless existing_room
+    @area[y + 2][x + 1] = '?' unless @area[y + 2][x + 1] == '|'
+    @area[y + 3][x - 1] = '#'
+    @area[y + 3][x] = '?' unless @area[y + 3][x] == '-'
+    @area[y + 3][x + 1] = '#'
+    
+    @area[y + 1][x] = '-'
+    [x, y + 2, 0, 0, existing_room]
+  end
+
+  def extend_w!(x, y)
+    ext_x = 0
+    if at_w_edge?(x, y)
+      @area.each{ |r|  r.prepend("  ") }
+      ext_x = 2
+      x += 2
+    end
+
+    existing_room = @area[y][x - 2] == '.'
+
+    @area[y - 1][x - 2] = '?' unless @area[y - 1][x - 2] == '-'
+    @area[y][x - 2] = '.' unless existing_room
+    @area[y + 1][x - 2] = '?' unless @area[y + 1][x - 2] == '-'
+    @area[y - 1][x - 3] = '#'
+    @area[y][x - 3] = '?' unless @area[y][x - 3] == '|'
+    @area[y + 1][x - 3] = '#'
+    
+    @area[y][x - 1] = '|'
+    [x - 2, y, ext_x, 0, existing_room]
+  end
+
+  def at_n_edge?(x, y)
+    y == 1
+  end
+
+  def at_e_edge?(x, y)
+    x == area.first.size - 2
+  end
+
+  def at_s_edge?(x, y)
+    y == area.size - 2
+  end
+
+  def at_w_edge?(x, y)
+    x == 1
+  end
+
+  def to_s(pretty = false)
+    pretty ? @area.map{ |r| r.gsub(/[.|-]/, ' ').gsub('#', '█') }.join("\n") : @area.join("\n")
+  end
+
+  def unknown_to_wall!
+    @area.each{ |r| r.gsub!('?', '#') }
+  end
+
+end
+
+area = Area.new("#?#?X?#?#".chars.each_slice(3).map(&:join))
+
+def find_closing_bracket(path, open_idx)
+  close_idx = open_idx
+  c = 1
+
+  while c > 0 do
+    close_idx += 1
+    case path[close_idx]
+    when '('
+      c += 1
+    when ')'
+      c -= 1
+    end
+  end
+
+  close_idx
+end
+
+$paths = []
+$DEBUG = false
+def build(area, path, x, y, steps)
+  pointer = 0
+  start_x, start_y = x, y
+  ext_x, ext_y = 0, 0
+  start_steps = steps
+
+  puts path if $DEBUG
+
+  while pointer < path.size do
+
+    item = path[pointer]
+    puts "#{path} #{pointer} #{item}" if $DEBUG
+
+    case item
+    when '('
+      branch_end = find_closing_bracket(path, pointer)
+      x, y = build(area, path[pointer+1...branch_end], x, y, steps)
+      pointer = branch_end
+    when ')'
+      pointer += 1
+      $paths << steps
+      steps = start_steps
+    when '|'
+      x, y = start_x + ext_x, start_y + ext_y
+      $paths << steps
+      steps = start_steps
+      puts "#{start_x} #{start_y} #{ext_x} #{ext_y}" if $DEBUG
+    else
+      x, y, e_x, e_y, existing_room = area.send("extend_#{item.downcase}!", x, y)
+      ext_x += e_x
+      ext_y += e_y
+      steps += 1 unless existing_room
+    end
+    
+    puts area if $DEBUG
+    pointer += 1
+  end
+
+  $paths << steps
+
+  $DEBUG = true if pointer == 10221 && x == 89 && y == 5
+  $DEBUG = false if pointer == 6 && x == 107 && y == 17
+
+  [x, y]
+end
+
+
+build(area, data, 1, 1, 0)
+area.unknown_to_wall!
+
+p $paths.max
+# p $paths.count{ |p| p >= 1000 }
