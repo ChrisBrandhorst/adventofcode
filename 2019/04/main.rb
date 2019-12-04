eb96bf1e019cb47541b68a98f05813d79cc315db
Chris Brandhorst <chris.brandhorst@topicus.nl>
2019-12-04 08:58:51 +0100

    Day 4

---
+++ 2019/04/main.rb
@@ -0,0 +1,35 @@
+input = File.read("input").split("-").map(&:to_i)
+
+def run(passes)
+  part1 = 0
+  part2 = 0
+
+  passes.each do |pass|
+    part1_ok = part2_ok = false
+
+    next if pass.sort != pass
+
+    pass.each do |dec|
+      c = pass.count(dec)
+      part1_ok = c >= 2 if !part1_ok
+      part2_ok = c == 2 if !part2_ok
+      break if part1_ok && part2_ok
+    end
+
+    part1 += 1 if part1_ok
+    part2 += 1 if part2_ok
+  end
+
+  [part1, part2]
+end
+
+start = Time.now
+passes = (input[0]..input[1]).map(&:to_s).map{ |pass| pass.chars.map(&:to_i) }
+puts "Building: #{Time.now-start}s"
+
+start = Time.now
+part1, part2 = run(passes)
+puts "Checking: #{Time.now - start}s"
+
+puts "Part 1: #{part1}"
+puts "Part 2: #{part2}"
