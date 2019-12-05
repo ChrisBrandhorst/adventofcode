d2ba7cf0fc81b6a6389d2def53687a5e8fbc235e
Chris Brandhorst <chris.brandhorst@topicus.nl>
2019-12-05 08:45:17 +0100

    Day 5: new Intcode, also slightly changing Day 2

--- 2019/05/main.rb
+++ 2019/05/main.rb
@@ -5,18 +5,11 @@ input = File.read("input").split(",").map(&:to_i)
 intcode = Intcode.new(input)
 
 start = Time.now
-part1 = intcode.with_nv(12,2).run
+part1 = intcode.with_input(1).run
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
+puts "\n"
+
 start = Time.now
-part2 = nil
-(0..99).each do |v|
-  (0..99).each do |n|
-    if intcode.with_nv(n,v).run == 19690720
-      part2 = 100 * n + v
-      break
-    end
-  end
-  break if !part2.nil?
-end
+part2 = intcode.with_input(5).run
 puts "Part 2: #{part2} (#{Time.now - start}s)"
--- 2019/intcode/intcode.rb
+++ 2019/intcode/intcode.rb
@@ -12,24 +12,76 @@ class Intcode
     @mem[addr]
   end
 
-  def run(noun, verb)
+  def with_input(inp)
+    @input = inp
+    self
+  end
+
+  def with_nv(noun, verb)
+    @noun = noun
+    @verb = verb
+    self
+  end
+
+  def run
     ptr = 0
     wm = @mem.clone
-    wm[1] = noun
-    wm[2] = verb
+
+    wm[1] = @noun unless @noun.nil?
+    wm[2] = @verb unless @verb.nil?
+
+    last_output = nil
+
     loop do
-      case wm[ptr]
+      opcode, p1, p2, out = get_instruction(wm, ptr)
+
+      case opcode
       when 1
-        wm[wm[ptr+3]] = wm[wm[ptr+1]] + wm[wm[ptr+2]]
+        wm[out] = p1 + p2
         ptr += 4
       when 2
-        wm[wm[ptr+3]] = wm[wm[ptr+1]] * wm[wm[ptr+2]]
+        wm[out] = p1 * p2
+        ptr += 4
+      when 3
+        puts "Input: #{@input}"
+        wm[out] = @input
+        ptr += 2
+      when 4
+        puts "Output: #{p1}"
+        last_output = p1
+        ptr += 2
+      when 5
+        ptr = p1 != 0 ? p2 : ptr + 3
+      when 6
+        ptr = p1 == 0 ? p2 : ptr + 3
+      when 7
+        wm[out] = p1 < p2 ? 1 : 0
+        ptr += 4
+      when 8
+        wm[out] = p1 == p2 ? 1 : 0
         ptr += 4
       when 99
         break
       end
     end
-    wm[0]
+    last_output || wm[0]
+  end
+
+  def get_instruction(wm, ptr)
+    opcode = wm[ptr] > 9 ? wm[ptr] % 100 : wm[ptr]
+    modes = wm[ptr] / 100
+    [
+      opcode,
+      get_param(wm, ptr, modes, 1),
+      get_param(wm, ptr, modes, 2),
+      opcode == 3 ? wm[ptr+1] : wm[ptr+3]
+    ]
+  end
+
+  def get_param(wm, ptr, modes, n)
+    val = wm[ptr+n]
+    mode = n == 1 ? modes % 10 : modes / 10
+    val.nil? || mode == 1 ? val : wm[val]
   end
 
 end
