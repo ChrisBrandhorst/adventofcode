3a2b345cb557ddf020daa8490ef1b31c93f81b11
Chris Brandhorst <chris.brandhorst@topicus.nl>
2019-12-06 07:13:58 +0100

    Day 6 part 2

--- 2019/06/main.rb
+++ 2019/06/main.rb
@@ -1,3 +1,5 @@
+require_relative 'dijkstra'
+
 input = File.readlines("input").map{ |l| l.strip.split(")") }
 
 class SpaceObject
@@ -23,22 +25,18 @@ class SpaceObject
     "Parent: #{@parent.nil? ? "-" : @parent.code}, Orbitee: [#{@orbitee.map{ |o| o.code }.join(",")}]"
   end
 
-end
+  def neighbours
+    if @parent.nil?
+      @orbitee
+    else
+      @orbitee + [@parent]
+    end
+  end
 
-objects = {}
+  def distance_to(other)
+    1
+  end
 
-input.each do |orb|
-  code = orb[0]
-  orbitee_code = orb[1]
-
-  obj = objects[code] || SpaceObject.new(code)
-  orbitee = objects[orbitee_code] || SpaceObject.new(orbitee_code)
-
-  orbitee.parent = obj
-
-  objects[code] = obj
-  objects[orbitee_code] = orbitee
-  obj << orbitee
 end
 
 def count_orbits(obj, start = 0)
@@ -46,6 +44,26 @@ def count_orbits(obj, start = 0)
 end
 
 start = Time.now
+objects = {}
+input.each do |orb|
+  code = orb[0]
+  orbitee_code = orb[1]
+
+  obj = objects[code] || SpaceObject.new(code)
+  orbitee = objects[orbitee_code] || SpaceObject.new(orbitee_code)
+
+  obj << orbitee
+  orbitee.parent = obj
+
+  objects[code] = obj
+  objects[orbitee_code] = orbitee
+end
+puts "Building: #{Time.now-start}s"
+
+start = Time.now
 part1 = count_orbits(objects["COM"])
 puts "Part 1: #{part1} (#{Time.now-start}s)"
 
+start = Time.now
+part2 = dijkstra_shortest_path(objects.values, objects["YOU"].parent, objects["SAN"].parent).size - 1
+puts "Part 2: #{part2} (#{Time.now-start}s)"
