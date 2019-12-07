7689cabe41f6299bb3f3e67c08be7eff8f4160b8
Chris Brandhorst <chris.brandhorst@topicus.nl>
2019-12-07 10:29:15 +0100

    Day 7 - Halfway cleanup

--- 2019/02/main.rb
+++ 2019/02/main.rb
@@ -5,14 +5,14 @@ input = File.read("input").split(",").map(&:to_i)
 intcode = Intcode.new(input)
 
 start = Time.now
-part1 = intcode.with_nv(12,2).run
+part1 = intcode.with_nv(12,2).run[0]
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
 part2 = nil
 (0..99).each do |v|
   (0..99).each do |n|
-    if intcode.with_nv(n,v).run == 19690720
+    if intcode.reset!.with_nv(n,v).run[0] == 19690720
       part2 = 100 * n + v
       break
     end
