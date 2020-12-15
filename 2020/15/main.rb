fd8e3977df2ad84558d5f45681c4209501cfe66f
Chris Brandhorst <chris.brandhorst@topicus.nl>
2020-12-15 17:18:00 +0100

    Day 15: Sloooow

---
+++ 2020/15/main.rb
@@ -0,0 +1,54 @@
+start = Time.now
+input = File.read("input").split(',').map(&:to_i)
+puts "Prep:   #{Time.now - start}s"
+
+start1 = Time.now
+spoken = [] | input
+
+until spoken.size == 2020
+  last = spoken.last
+  count = spoken.count(last)
+
+  if count == 1
+    spoken << 0
+    # puts "#{spoken.size}: 0"
+  else
+    spoken[spoken.size-1] = -1
+    ri = spoken.rindex(last)
+    spoken[spoken.size-1] = last
+    nl = (spoken.size) - (ri + 1)
+    spoken << nl
+    # puts "#{spoken.size}: #{nl}"
+  end
+end
+
+part1 = spoken.last
+puts "Part 1: #{part1} (#{Time.now - start1}s)"
+
+start2 = Time.now
+
+spoken = input.map.with_index{ |a,i| [a,[i]] }.to_h
+count = spoken.keys.size
+last = input.last
+
+until count == 30000000
+  if spoken[last] && spoken[last].size == 1
+    (spoken[0] ||= []) << count
+    last = 0
+  else
+    nl = count - spoken[last][-2] + 1
+    (spoken[nl] ||= []) << count
+    last = nl
+  end
+  count += 1
+  puts count if count % 100000 == 0
+end
+
+part2 = last
+
+p spoken.values.map{|a|a.size}
+
+
+puts "Part 2: #{part2} (#{Time.now - start2}s)"
+
+puts "Total:  #{Time.now - start}s"
