ef0d8fb5745f773761fa38515f11509da1f36de4
Chris Brandhorst <chris.brandhorst@topicus.nl>
2020-12-23 13:13:57 +0100

    Day 23: More readable

--- 2020/23/main.rb
+++ 2020/23/main.rb
@@ -1,57 +1,71 @@
 PRV = 0
 NXT = 1
 
-def game(circle, length = 100)
-  prt = length > 100000
-  min = circle.min
-  max = circle.max
+class Cup
+  attr_reader :label
+  attr_accessor :prv, :nxt
 
-  h = circle.each_with_index.inject({}) do |h,(l,i)|
-    h[l] = [
-      circle[i - 1],
-      circle[i + 1] || circle.first
-    ]; h
+  def initialize(label)
+    @label = label
   end
 
-  cur = circle.first
-  length.times do |i|
-    print "." if prt && i % 100000 == 0
-
-    nxt = [
-      h[cur][NXT],
-      h[h[cur][NXT]][NXT],
-      h[h[h[cur][NXT]][NXT]][NXT]
-    ]
-
-    dst = cur - 1
-    until dst >= min && !nxt.include?(dst)
-      dst -= 1
-      dst = max if dst < min
-    end
-
-    cur_nxt = h[nxt[2]][NXT]
-    aft = h[dst][NXT]
-
-    # Remove nxt from circle
-    h[cur][NXT] = cur_nxt
-    h[cur_nxt][PRV] = cur
-
-    # Add nxt back to circle
-    h[dst][NXT] = nxt[0]
-    h[aft][PRV] = nxt[2]
-
-    h[nxt[0]][PRV] = dst
-    h[nxt[2]][NXT] = aft
-
-    cur = h[cur][NXT]
+  def place_after(other)
+    @prv = other
+    other.nxt = self
   end
-
-  print " Done\n" if prt
-  h
 end
 
-def order(h)
-  (h.count-1).times.inject([1]){ |o| o << h[o.last][NXT] }
+def game(circle, length = 100)
+  do_prt = length > 100000
+  min, max = circle.min, circle.max
+
+  # Build hash of cups with neighbours
+  cups = circle.each_with_index.inject({}) do |cups,(l,i)|
+    pl = circle[i - 1]
+    nl = circle[i + 1] || circle.first
+    cup = cups[l] ||= Cup.new(l)
+    cup.prv = cups[pl] ||= Cup.new(pl)
+    cup.nxt = cups[nl] ||= Cup.new(nl)
+    cups
+  end
+
+  cur = cups[circle.first]
+  length.times do |i|
+    print "." if do_prt && i % 100000 == 0
+
+    # Get picks and labels
+    pick_first, pick_last = cur.nxt, cur.nxt.nxt.nxt
+    pick_labels = [
+      pick_first.label,
+      pick_first.nxt.label,
+      pick_last.label
+    ]
+
+    # Find destination
+    dst_label = cur.label - 1
+    until dst_label >= min && !pick_labels.include?(dst_label)
+      dst_label = dst_label - 1 < min ? max : dst_label - 1
+    end
+    dst = cups[dst_label]
+
+    # Remove picks from circle
+    pick_last.nxt.place_after(cur)
+
+    # Add pick back to circle
+    aft_dst = dst.nxt
+    pick_first.place_after(dst)
+    aft_dst.place_after(pick_last)
+
+    # Go next
+    cur = cur.nxt
+  end
+
+  print " Done\n" if do_prt
+  cups
+end
+
+def order(cups)
+  (cups.count-1).times.inject([ cups[1] ]){ |o| o << o.last.nxt }
 end
 
 start = Time.now
@@ -60,29 +74,14 @@ puts "Prep:   #{Time.now - start}s"
 
 start1 = Time.now
 result = game(input.dup)
-part1 = order(result).drop(1).join('')
+part1 = order(result).drop(1).map(&:label).join('')
 puts "Part 1: #{part1} (#{Time.now - start1}s)"
 
 start2 = Time.now
 circle = input.dup | (input.max+1..1000000).to_a
 result = game(circle, 10000000)
-n1 = result[1][NXT]
-part2 = n1 * result[n1][NXT]
+n1 = result[1].nxt
+part2 = n1.label * n1.nxt.label
 puts "Part 2: #{part2} (#{Time.now - start2}s)"
 
 puts "Total:  #{Time.now - start}s"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
