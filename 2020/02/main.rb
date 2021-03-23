f252cc9e8eb69798c911fe80af9f40f1e55b0f81
Chris Brandhorst <chris.brandhorst@topicus.nl>
2021-03-23 17:36:10 +0100

    Day 02: Improvements

--- 2020/02/main.rb
+++ 2020/02/main.rb
@@ -1,15 +1,19 @@
 RULE_FORMAT = /^(\d+)-(\d+) (\w): (\w+)$/
+MIN = 0
+MAX = 1
+CHAR = 2
+PASS = 3
 
 start = Time.now
 input = File.readlines("input")
   .map{ |r| r.match(RULE_FORMAT).captures }
-  .each{ |r| r[0] = r[0].to_i; r[1] = r[1].to_i }
+  .each{ |r| r[MIN] = r[MIN].to_i; r[MAX] = r[MAX].to_i }
 puts "Prep: #{Time.now - start}s"
 
 start = Time.now
-part1 = input.map{ |r| c = r[3].count(r[2]); c >= r[0] && c <= r[1] }.count(true)
+part1 = input.select{ |r| r[PASS].count(r[CHAR]).between?(r[MIN], r[MAX]) }.size
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-part2 = input.map{ |r| (r[3][r[0] - 1] == r[2]) ^ (r[3][r[1] - 1] == r[2]) }.count(true)
+part2 = input.select{ |r| (r[PASS][r[MIN] - 1] == r[CHAR]) ^ (r[PASS][r[MAX] - 1] == r[CHAR]) }.size
 puts "Part 2: #{part2} (#{Time.now - start}s)"
