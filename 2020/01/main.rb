6c10eeabaf5af97790c24b9bdfb87b228e94cac7
Chris Brandhorst <chris.brandhorst@topicus.nl>
2021-06-15 13:27:00 +0200

    Day 1 improvement

--- 2020/01/main.rb
+++ 2020/01/main.rb
@@ -2,16 +2,14 @@ start = Time.now
 input = File.readlines("input").map(&:to_i)
 puts "Prep: #{Time.now - start}s"
 
-start = Time.now
-part1 = nil
-input.each do |i|
-  input.each do |j|
-    part1 = i * j if i + j == 2020
-  end
-  break unless part1.nil?
+def find_entries(input, n, s)
+  input.combination(n).find{ |c| c.sum == s }.inject(:*)
 end
+
+start = Time.now
+part1 = find_entries(input, 2, 2020)
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-part2 = input.combination(3).find{ |a,b,c| a + b + c == 2020 }.inject(:*)
+part2 = find_entries(input, 3, 2020)
 puts "Part 2: #{part2} (#{Time.now - start}s)"
