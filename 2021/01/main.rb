f22d0c1f922479b547c64e8f8c1a90dd935c2ba8
Chris Brandhorst <chris.brandhorst@topicus.nl>
2021-12-01 11:09:17 +0100

    More cleaner

--- 2021/01/main.rb
+++ 2021/01/main.rb
@@ -2,14 +2,14 @@ start = Time.now
 input = File.readlines("input", chomp: true).map(&:to_i)
 puts "Prep: #{Time.now - start}s"
 
-def count_larger(inp)
-  inp.each_cons(2).count{ |a,b| b > a }
+def count_larger(inp, window)
+  inp.each_cons(window).count{ |a| a.last > a.first }
 end
 
 start = Time.now
-part1 = count_larger(input)
+part1 = count_larger(input, 2)
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-part2 = count_larger(input.each_cons(3).map(&:sum))
+part2 = count_larger(input, 4)
 puts "Part 2: #{part2} (#{Time.now - start}s)"
