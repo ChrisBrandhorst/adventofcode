43bd7b49e945e0a50db4a5f11b666cd201642a17
Chris Brandhorst <chris.brandhorst@topicus.nl>
2021-12-01 06:12:09 +0100

    Day 01

---
+++ 2021/01/main.rb
@@ -0,0 +1,15 @@
+start = Time.now
+input = File.readlines("input", chomp: true).map(&:to_i)
+puts "Prep: #{Time.now - start}s"
+
+def count_larger(inp)
+  inp.each_cons(2).map{ |a,b| b > a }.count(true)
+end
+
+start = Time.now
+part1 = count_larger(input)
+puts "Part 1: #{part1} (#{Time.now - start}s)"
+
+start = Time.now
+part2 = count_larger(input.each_cons(3).map(&:sum))
+puts "Part 2: #{part2} (#{Time.now - start}s)"
