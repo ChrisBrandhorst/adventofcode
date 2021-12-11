fce018767a8ef108611332586785344c3131f006
Chris Brandhorst <chris.brandhorst@topicus.nl>
2021-12-11 08:07:41 +0100

    Template

---
+++ 2021/template/main.rb
@@ -0,0 +1,13 @@
+start = Time.now
+input = File.readlines("input", chomp: true)
+puts "Prep: #{Time.now - start}s"
+
+
+start = Time.now
+part1 = nil
+puts "Part 1: #{part1} (#{Time.now - start}s)"
+
+
+start = Time.now
+part2 = nil
+puts "Part 2: #{part2} (#{Time.now - start}s)"
