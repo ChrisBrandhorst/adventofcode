355072ad4dddbf38701706779042681a69b71c31
Chris Brandhorst <chris.brandhorst@topicus.nl>
2021-12-14 07:26:29 +0100

    Day 14

---
+++ 2021/14/main.rb
@@ -0,0 +1,45 @@
+start = Time.now
+template, rules = File.read("input", chomp: true).split("\n\n")
+rules = rules.split("\n").map{ |r| r.split(" -> ") }.to_h
+puts "Prep: #{Time.now - start}s"
+
+
+def apply_rules(pairs, rules)
+  new_pairs = {}
+  pairs.each do |k,v|
+    insert = rules[k]
+    if insert
+      a, b = k[0] + insert, insert + k[-1]
+      new_pairs[a] = new_pairs[a] ? new_pairs[a] + v : v
+      new_pairs[b] = new_pairs[b] ? new_pairs[b] + v : v
+    end
+  end
+  new_pairs
+end
+
+def calc(template, rules, steps)
+  pairs = template.chars.each_cons(2).map(&:join).tally
+  pairs = steps.times.inject(pairs){ |pairs| apply_rules(pairs, rules) }
+
+  tally = {
+    template[0] => 1,
+    template[-1] => 1
+  }
+  pairs.each do |pair,t|
+    a, b = pair.chars
+    tally[a] = (tally[a] || 0) + t
+    tally[b] = (tally[b] || 0) + t
+  end
+
+  tally.values.max / 2 - tally.values.min / 2
+end
+
+
+start = Time.now
+part1 = calc(template, rules, 10)
+puts "Part 1: #{part1} (#{Time.now - start}s)"
+
+
+start = Time.now
+part2 = calc(template, rules, 40)
+puts "Part 2: #{part2} (#{Time.now - start}s)"
