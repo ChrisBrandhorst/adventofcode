ac51cf88ef7c6e120b08ad4acf66833d5d095bb7
Chris Brandhorst <chris.brandhorst@topicus.nl>
2021-12-20 07:23:31 +0100

    Day 20 + InfiniteGrid

---
+++ 2021/20/main.rb
@@ -0,0 +1,62 @@
+require_relative '../util/infinite_grid.rb'
+
+class ImageGrid < InfiniteGrid
+
+  attr_reader :min_x, :max_x
+  attr_reader :min_y, :max_y
+
+  def initialize(rows)
+    super(rows, '.', 3)
+  end
+
+  def adj_bin(x, y = nil)
+    x, y = *x if x.is_a?(Array)
+    pix = self.adj(x,y).map{ |a| a == '#' ? '1' : '0' }.join.to_i(2)
+  end
+
+  def step!(enhancement, reset_boundary = false)
+    new_pix = {}
+    self.each{ |c| new_pix[c] = enhancement[self.adj_bin(c)] }
+    
+    if reset_boundary
+      @min_x += 1
+      @max_x -= 1
+      @min_y += 1
+      @max_y -= 1
+    end
+
+    @points = {}
+    new_pix.each do |c,v|
+      self[c] = v if !reset_boundary || (c[0] >= @min_x && c[0] <= @max_x && c[1] >= @min_y && c[1] <= @max_y)
+    end
+  end
+
+  def count_lit
+    @points.count{ |k,v| v == '#' }
+  end
+
+end
+
+
+def step(grid, enhancement, n)
+  n.times { |i| grid.step!(enhancement, (i+1).even?) }
+  grid.count_lit
+end
+
+
+start = Time.now
+input = File.readlines("input", chomp: true)
+enhancement = input[0]
+data = input[2..-1].map{ |l| l.chars }
+grid = ImageGrid.new(data)
+puts "Prep: #{Time.now - start}s"
+
+
+start = Time.now
+part1 = step(grid, enhancement, 2)
+puts "Part 1: #{part1} (#{Time.now - start}s)"
+
+
+start = Time.now
+part2 = step(grid, enhancement, 48)
+puts "Part 2: #{part2} (#{Time.now - start}s)"
---
+++ 2021/util/infinite_grid.rb
@@ -0,0 +1,70 @@
+class InfiniteGrid
+
+  attr_reader :min_x, :max_x
+  attr_reader :min_y, :max_y
+  attr_accessor :lookaround, :empty_value
+
+  def initialize(rows, empty_value, lookaround = 1)
+    @points = {}
+    (0...rows.first.size).each do |y|
+      (0...rows.size).each do |x|
+        @points[[x,y]] = rows[y][x]
+      end
+    end
+
+    @min_x, @max_x = 0, rows.size - 1
+    @min_y, @max_y = 0, rows.first.size - 1
+    @empty_value = empty_value
+    @lookaround = lookaround
+  end
+
+  def [](x, y = nil)
+    x = [x,y] unless x.is_a?(Array)
+    @points[x] || @empty_value
+  end
+
+  def []=(x, y = nil, v)
+    x, v = [x,y], v unless x.is_a?(Array)
+    @min_x = x[0] if x[0] < @min_x
+    @max_x = x[0] if x[0] > @max_x
+    @min_y = x[1] if x[1] < @min_y
+    @max_y = x[1] if x[1] > @max_y
+    @points[x] = v
+  end
+
+  def each
+    (@min_y-@lookaround..@max_y+@lookaround).each do |y|
+      (@min_x-@lookaround..@max_x+@lookaround).each do |x|
+        yield([x,y], self[x,y]) if block_given?
+      end
+    end
+  end
+
+  def adj(x, y = nil)
+    x, y = *x if x.is_a?(Array)
+    ad = [
+      self[x-1,y-1],
+      self[x,y-1],
+      self[x+1,y-1],
+      self[x-1,y],
+      self[x,y],
+      self[x+1,y],
+      self[x-1,y+1],
+      self[x,y+1],
+      self[x+1,y+1]
+    ]
+  end
+
+  def flatten
+    @points.values
+  end
+
+  def inspect
+    (@min_y..@max_y).map do |y|
+      (@min_x..@max_x).map do |x|
+        self[x,y]
+      end.join
+    end.join("\n")
+  end
+
+end
