b67d4853bf3fa3fdf1c59d0326b871f44951d944
Chris Brandhorst <chris.brandhorst@topicus.nl>
2022-12-01 10:18:20 +0100

    Shorter

--- 2022/01/main.rb
+++ 2022/01/main.rb
@@ -1,14 +1,14 @@
 start = Time.now
-input = File.readlines("input", chomp: true)
-  .slice_before("")
-  .map{ |i| i.sum(&:to_i) }
-  .sort
+$/*=2 # Set global line delimiter to \n\n
+input = File.readlines("input")
+  .map{ _1.split.sum(&:to_i) } # _1 is first argument of block. split works on \n
+  .max(3)
 puts "Prep: #{Time.now - start}s"
 
 start = Time.now
-part1 = input.last
+part1 = input.first
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-part2 = input.last(3).sum
+part2 = input.sum
 puts "Part 2: #{part2} (#{Time.now - start}s)"
