1a2ffee03fa8afac84e7efbdebadf4346efb705e
Chris Brandhorst <chris.brandhorst@topicus.nl>
2022-12-05 13:35:36 +0100

    Day 05 - Dup bug

--- 2022/05/main.rb
+++ 2022/05/main.rb
@@ -7,13 +7,13 @@ moves.map!{ |m| m.scan(/\d+/).map(&:to_i) }
 puts "Prep: #{Time.now - start}s"
 
 start = Time.now
-stacks1 = stacks.dup
+stacks1 = stacks.map(&:dup)
 moves.each{ |count, from, to| count.times{ stacks1[to-1] << stacks1[from-1].pop } }
 part1 = stacks1.map(&:last).join
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-stacks2 = stacks.dup
+stacks2 = stacks.map(&:dup)
 moves.each{ |count, from, to| stacks2[to-1] += stacks2[from-1].pop(count) }
 part2 = stacks2.map(&:last).join
 puts "Part 2: #{part2} (#{Time.now - start}s)"
