ec337dff3a723f64049bedfd93639b1623cf17fc
Chris Brandhorst <chris.brandhorst@topicus.nl>
2022-12-05 07:52:55 +0100

    Day 05 - Small fix

--- 2022/05/main.rb
+++ 2022/05/main.rb
@@ -7,7 +7,7 @@ input.each do |i|
   if i[0] == "m"
     moves << i.scan(/\d+/).map(&:to_i)
   else
-    rows << i.chars.select.with_index{ |_,j| (j - 1) % 4 == 0}
+    rows << i.chars.select.with_index{ |_,j| (j - 1) % 4 == 0 }
   end
 end
 
@@ -22,6 +22,6 @@ puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
 stacks2 = stacks.dup
-moves.each{ |count, from, to| stacks2[to-1] = stacks2[to-1] + stacks2[from-1].pop(count) }
+moves.each{ |count, from, to| stacks2[to-1] += stacks2[from-1].pop(count) }
 part2 = stacks2.map(&:last).join
 puts "Part 2: #{part2} (#{Time.now - start}s)"
