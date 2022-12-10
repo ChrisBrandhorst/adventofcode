f21305d02909bb620958dcbc65971d465aaf547a
Chris Brandhorst <chris.brandhorst@topicus.nl>
2022-12-10 21:13:49 +0100

    Day 10 - Faster

--- 2022/10/main.rb
+++ 2022/10/main.rb
@@ -15,8 +15,8 @@ part1 = [20,60,100,140,180,220].sum{ |c| c * cycle_value[c-1] }
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-image = cycle_value[0..-2].each_with_index.inject("") do |im, (v,i)|
-  im += ((v - (i % 40) ).abs > 1 ? "." : '#')
+image = (0..cycle_value.size-2).inject("") do |im,i|
+  im += ((cycle_value[i] - (i % 40) ).abs > 1 ? "." : '#')
   (i + 1) % 40 == 0 ? im + "\n" : im
 end
 puts image
