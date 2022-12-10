d513fb518c3103821fd9825026fa7e1491a45624
Chris Brandhorst <chris.brandhorst@topicus.nl>
2022-12-10 21:22:20 +0100

    Day 10 - Shorter

--- 2022/10/main.rb
+++ 2022/10/main.rb
@@ -15,10 +15,7 @@ part1 = [20,60,100,140,180,220].sum{ |c| c * cycle_value[c-1] }
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-image = (0..cycle_value.size-2).inject("") do |im,i|
-  im += ((cycle_value[i] - (i % 40) ).abs > 1 ? "." : '#')
-  (i + 1) % 40 == 0 ? im + "\n" : im
-end
-puts image
+pixels = (0..cycle_value.size-2).map{ |i| (cycle_value[i] - (i % 40) ).abs > 1 ? "." : '#' }
+puts pixels.each_slice(40).map(&:join)
 part2 = nil
 puts "Part 2: #{part2} (#{Time.now - start}s)"
