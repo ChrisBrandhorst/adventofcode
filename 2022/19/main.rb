a7e0818bdfd089534b5479f619ada8728e4d43ff
Chris Brandhorst <chris.brandhorst@topicus.nl>
2022-12-20 08:53:18 +0100

    Day 19 - Bit faster

--- 2022/19/main.rb
+++ 2022/19/main.rb
@@ -31,22 +31,27 @@ def produce!(state)
   state[:m_ore] += state[:r_ore]
   state[:m_clay] += state[:r_clay]
   state[:m_obs] += state[:r_obs]
-  state[:geo] += state[:r_geo]
+  state[:m_geo] += state[:r_geo]
 end
 
 def bfs(blueprint, start_state, max_time)
 
   q = [start_state]
   final_states = []
-  max_geo = 0
 
   max_r_ore = [blueprint[:ore][:ore], blueprint[:clay][:ore], blueprint[:obs][:ore], blueprint[:geo][:ore]].max
   max_r_clay = blueprint[:obs][:clay]
   max_r_obs = blueprint[:geo][:obs]
 
+  visited = Set.new
+
   until q.empty?
-    prev_state = q.shift
-    state = prev_state.dup
+    state = q.shift.dup
+
+    vstate = state.dup
+    vstate.delete(:time)
+    next if visited.include?(vstate)
+    visited << vstate
 
     # Time
     state[:time] += 1
@@ -55,13 +60,8 @@ def bfs(blueprint, start_state, max_time)
     if state[:time] == max_time
       produce!(state)
       final_states << state
-      max_geo = [max_geo, state[:geo]].max
       next
     end
-    
-    # Relevant?
-    rem_time = max_time - state[:time]
-    next if rem_time * state[:geo] + (1..rem_time).sum < max_geo
 
     # Split
     bs = nil
@@ -71,7 +71,6 @@ def bfs(blueprint, start_state, max_time)
       bs[:m_obs] -= blueprint[:geo][:obs]
       produce!(bs)
       bs[:r_geo] += 1
-      bs[:prev] = state
       q << bs
     else
       if state[:r_obs] < max_r_obs && state[:m_ore] >= blueprint[:obs][:ore] && state[:m_clay] >= blueprint[:obs][:clay]
@@ -80,7 +79,6 @@ def bfs(blueprint, start_state, max_time)
         bs[:m_clay] -= blueprint[:obs][:clay]
         produce!(bs)
         bs[:r_obs] += 1
-        bs[:prev] = state
         q << bs
       else
         if state[:r_clay] < max_r_clay && state[:m_ore] >= blueprint[:clay][:ore]
@@ -88,7 +86,6 @@ def bfs(blueprint, start_state, max_time)
           bs[:m_ore] -= blueprint[:clay][:ore]
           produce!(bs)
           bs[:r_clay] += 1
-          bs[:prev] = state
           q << bs
         end
         if state[:r_ore] < max_r_ore && state[:m_ore] >= blueprint[:ore][:ore]
@@ -96,7 +93,6 @@ def bfs(blueprint, start_state, max_time)
           bs[:m_ore] -= blueprint[:ore][:ore]
           produce!(bs)
           bs[:r_ore] += 1
-          bs[:prev] = state
           q << bs
         end
       end
@@ -115,7 +111,6 @@ end
 
 start_state = {
   time: 0,
-  geo: 0,
   r_ore: 1,
   r_clay: 0,
   r_obs: 0,
@@ -123,12 +118,12 @@ start_state = {
   m_ore: 0,
   m_clay: 0,
   m_obs: 0,
-  prev: nil
+  m_geo: 0
 }
 
-part1 = input.map{ |i| i[:id] * bfs(i, start_state.dup, 24).map{ _1[:geo] }.max }.sum
+part1 = input.map{ |i| i[:id] * bfs(i, start_state.dup, 24).map{ _1[:m_geo] }.max }.sum
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-part2 = input[0..2].map{ |i| bfs(i, start_state.dup, 32).map{ _1[:geo] }.max }.inject(:*)
+part2 = input[0..2].map{ |i| bfs(i, start_state.dup, 32).map{ _1[:m_geo] }.max }.inject(:*)
 puts "Part 2: #{part2} (#{Time.now - start}s)"
