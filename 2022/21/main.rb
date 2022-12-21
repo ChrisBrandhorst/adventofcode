0f8198d2cd27d32c48d6a2bce9cb192321d52b91
Chris Brandhorst <chris.brandhorst@topicus.nl>
2022-12-21 22:06:37 +0100

    Day 21 - Betterder

--- 2022/21/main.rb
+++ 2022/21/main.rb
@@ -1,21 +1,24 @@
+require 'bigdecimal'
+
 start = Time.now
 input = File.readlines("input", chomp: true)
   .map{ _1.match(/^(\w+): ((\d+)|(\w+) (.) (\w+))$/).captures }
-  .inject({}){ |m,l| m[l[0]] = l[2] ? l[2].to_f : [l[3],l[4].to_sym,l[5]]; m }
+  .to_h{ |l| [l[0].to_sym, l[2] ? BigDecimal(l[2]) : l[3..5].map(&:to_sym)] }
 puts "Prep: #{Time.now - start}s"
 
-def calc(input, target = "root")
+def calc(input, humn = nil)
   todo = input.keys
-  vals = todo.inject({}){ |m,l| m[l] = nil; m }
-  until todo.empty?
+  vals = {}
+  vals[:humn] = humn unless humn.nil?
 
+  until todo.empty?
     k = todo.shift
-    if vals[k].is_a?(Float)
+    if vals.key?(k)
       next
-    elsif input[k].is_a?(Float)
+    elsif input[k].is_a?(Numeric)
       vals[k] = input[k]
-    elsif vals[input[k][0]].is_a?(Float) && vals[input[k][2]].is_a?(Float)
-      if k == target
+    elsif vals.key?(input[k][0]) && vals.key?(input[k][2])
+      if k == :root
         return [vals[input[k][0]], vals[input[k][2]]]
       else
         vals[k] = vals[input[k][0]].send( input[k][1], vals[input[k][2]] )
@@ -29,32 +32,18 @@ end
 
 
 start = Time.now
-sol = calc(input)
-part1 = sol.sum.to_i
+part1 = (part1_sol = calc(input)).sum.to_i
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 
 start = Time.now
 
-# Find upper bound
-until sol[0] - sol[1] < 0
-  l = input["humn"]
-  input["humn"] = (input["humn"] * 2).floor.to_f
-  r = input["humn"]
-  sol = calc(input)
-end
+x1, y1 = input[:humn], part1_sol[0]
+x2, y2 = input[:humn] += 1, calc(input)[0]
 
-# Binary search
-until sol[0] - sol[1] == 0
-  input["humn"] = ((l + r) / 2).floor.to_f
-  sol = calc(input)
+d = (part1_sol[1] - y2)
+dx = (x2 - x1)
+dy = (y2 - y1)
 
-  if sol[0] > sol[1]
-    l = input["humn"]
-  elsif sol[0] < sol[1]
-    r = input["humn"]
-  end
-end
-
-part2 = input["humn"].to_i
+part2 = (d / dy * dx + x2).round
 puts "Part 2: #{part2} (#{Time.now - start}s)"
