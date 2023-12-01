300f399c28351d4eba1ac94d8fa53dd718dfd389
Chris Brandhorst <chris.brandhorst@gmail.com>
2023-12-01 08:43:21 +0100

    Day 02: Better

--- 2023/01/main.rb
+++ 2023/01/main.rb
@@ -5,16 +5,17 @@ class String
 end
 
 INT_MAP = {
-  "one" => 1,
-  "two" => 2,
-  "three" => 3,
-  "four" => 4,
-  "five" => 5,
-  "six" => 6,
-  "seven" => 7,
-  "eight" => 8,
-  "nine" => 9
+  "one" => "1",
+  "two" => "2",
+  "three" => "3",
+  "four" => "4",
+  "five" => "5",
+  "six" => "6",
+  "seven" => "7",
+  "eight" => "8",
+  "nine" => "9"
 }
+MIN_INT_LENGTH = INT_MAP.keys.map(&:length).min
 
 start = Time.now
 input = File.read("input").split("\n").map(&:chars)
@@ -28,32 +29,20 @@ start = Time.now
 
 part2 = File.read("input").split("\n").sum do |l|
 
-  done = false
+  first, last = nil
+
   (0...l.size).each do |i|
-    break if l[i].is_int? || done
+    first = l[i]            if first.nil? && l[i].is_int?
+    last  = l[l.length-i-1] if last.nil?  && l[l.length-i-1].is_int?
+    
     INT_MAP.each do |k,v|
-      if l[i,k.length] == k
-        l.sub!(k, v.to_s)
-        done = true
-        break
-      end
+      first = v if first.nil? && l[i,k.length] == k
+      last  = v if last.nil?  && l[l.length-i-1,k.length] == k
     end
+    break if first && last
   end
 
-  done = false
-  (0...l.size).each do |i|
-    break if l[l.length-i-1].is_int? || done
-    INT_MAP.each do |k,v|
-      if l[l.length-i-1,k.length] == k
-        l = l.reverse.sub(k.reverse, v.to_s).reverse
-        done = true
-        break
-      end
-    end
-  end
-  
-  cs = l.chars
-  (cs.find{ _1.is_int? } + cs.reverse.find{ _1.is_int? }).to_i
+  (first + last).to_i
 end
 
 puts "Part 2: #{part2} (#{Time.now - start}s)"
