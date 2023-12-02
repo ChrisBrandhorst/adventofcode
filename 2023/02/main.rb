8a3dbfb7611dcf060c3346a5dd86473f6f7be8cc
Chris Brandhorst <chris.brandhorst@gmail.com>
2023-12-02 07:14:04 +0100

    Day 02

---
+++ 2023/02/main.rb
@@ -0,0 +1,15 @@
+start = Time.now
+input = File.readlines("input", chomp: true)
+  .map{ _1.split(": ").last.split("; ").map{ |c| c.split(", ").map{ |d| e = d.split(" "); [e.last.to_sym, e.first.to_i] }.to_h } }
+puts "Prep: #{Time.now - start}s"
+
+
+start = Time.now
+MAXES = {red: 12, green: 13, blue: 14}
+part1 = input.each_with_index.sum{ |g,i| !g.any?{ |r| r.detect{ |k,v| v > MAXES[k] } } ? i + 1 : 0 }
+puts "Part 1: #{part1} (#{Time.now - start}s)"
+
+
+start = Time.now
+part2 = input.sum{ |g| g.inject{|a,b| a.merge(b){ |_,s,t| [s,t].max } }.values.inject(&:*) }
+puts "Part 2: #{part2} (#{Time.now - start}s)"
