8be2373cf98c4363a7ae27b8f55eab5c130c6963
Chris Brandhorst <chris.brandhorst@gmail.com>
2023-12-04 07:54:16 +0100

    Day 04

---
+++ 2023/04/main.rb
@@ -0,0 +1,14 @@
+start = Time.now
+input = File.readlines("input")
+  .map{ |l| l.split(": ").last.split(" | ").map{ _1.split(" ").map(&:to_i) }[0..-1].inject(&:intersection).size }
+puts "Prep: #{Time.now - start}s"
+
+
+start = Time.now
+part1 = input.sum{ _1 == 0 ? 0 : 2 ** (_1-1) }
+puts "Part 1: #{part1} (#{Time.now - start}s)"
+
+
+start = Time.now
+part2 = (cards = [1]*input.size).each_with_index.sum{ |m,i| (1..input[i]).each{ cards[i+_1] += m }; m }
+puts "Part 2: #{part2} (#{Time.now - start}s)"
