b497f8e7bce104c5f6973638eb7fe2e2d9c27e7d
Chris Brandhorst <chris.brandhorst@gmail.com>
2023-12-13 12:09:51 +0100

    Day 13: Much better

--- 2023/13/main.rb
+++ 2023/13/main.rb
@@ -2,67 +2,54 @@ require '../util/grid'
 
 
 start = Time.now
-input = File.readlines("input", chomp: true)
-patterns = input.inject([[]]) do |ps,l|
-  if l == ""
-    ps << []
-  else
-    ps.last << l
+input = File.read("input").split("\n\n").map(&:split).map do |inp|
+  grid = inp.map(&:chars)
+  rows, cols = [0] * grid.size, [0] * grid.first.size
+  grid.each_with_index do |row,y|
+    row.each_with_index do |v,x|
+      if v == "#"
+        rows[y] += 2 ** x
+        cols[x] += 2 ** y
+      end
+    end
   end
-  ps
+  [rows,cols]
 end
-patterns = patterns.map{ Grid.new(_1.map(&:chars)) }
 puts "Prep: #{Time.now - start}s"
 
 
-def get_mirror_lines(pattern, flip = nil, prmi = nil, pcmi = nil)
-  rows, cols = [0] * pattern.row_count, [0] * pattern.col_count
-  old_v = nil
-
-  if flip
-    old_v = pattern[flip]
-    pattern[flip] = old_v == "#" ? "." : "#"
-  end
-
-  pattern.each do |(x,y),v|
-    if v == "#"
-      rows[y] += 2 ** x
-      cols[x] += 2 ** y
+def get_mirror_line(arr, part1_i = nil)
+  (1...arr.size).detect do |i|
+    next if i == part1_i
+    min_length = [i, arr.size - i].min
+    arr[i-min_length...i].reverse.zip(arr[i,min_length]).all? do |a,b|
+      d = a ^ b
+      d == 0 || !part1_i.nil? && (d & (d - 1)) == 0
     end
   end
+end
 
-  pattern[flip] = old_v if flip
 
-  rmi = (1...rows.size).detect do |i|
-    next if i == prmi
-    min_length = [i, rows.size - i].min
-    rows[i-min_length...i].reverse == rows[i,min_length]
-  end
-  
-  if rmi.nil?
-    cmi = (1...cols.size).detect do |i|
-      next if i == pcmi
-      min_length = [i, cols.size - i].min
-      cols[i-min_length...i].reverse == cols[i,min_length]
-    end
-  end
+def get_mirror_lines(encoding, prev_lines = nil)
+  rows, cols = encoding
+  prmi, pcmi = prev_lines
+
+  rmi = get_mirror_line(rows, prmi)
+  cmi = get_mirror_line(cols, pcmi) if rmi.nil?
 
   [rmi || 0, cmi || 0]
 end
 
 
-
 start = Time.now
-pattern_mirrors = patterns.map{ get_mirror_lines(_1) }
-part1 = pattern_mirrors.sum{ _1 * 100 + _2 }
+mirror_lines = input.map{ get_mirror_lines(_1) }
+part1 = mirror_lines.sum{ _1 * 100 + _2 }
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 
 start = Time.now
-part2 = patterns.each_with_index.sum do |pattern,i|
-  pattern.each do |(x,y),v|
-    nrmi, ncmi = get_mirror_lines(pattern, [x,y], *pattern_mirrors[i])
-    break(nrmi * 100 + ncmi) if nrmi > 0 || ncmi > 0
-  end
+part2 = input.zip(mirror_lines).sum do |encoding,prev|
+  nrmi, ncmi = get_mirror_lines(encoding, prev)
+  nrmi * 100 + ncmi
 end
 puts "Part 2: #{part2} (#{Time.now - start}s)"
