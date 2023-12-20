5e8eeba87ddcff671dc3c74f14d53b89bb0422e9
Chris Brandhorst <chris.brandhorst@gmail.com>
2023-12-20 11:29:09 +0100

    Day 20: Booleans instead of symbols for pulses

--- 2023/20/main.rb
+++ 2023/20/main.rb
@@ -2,7 +2,7 @@ start = Time.now
 input = File.readlines("input", chomp: true)
 puts "Prep: #{Time.now - start}s"
 
-def reset_modules(input)
+def build_modules(input)
   modules = input.inject({}) do |mods,l|
     nt, targets = l.split(" -> ")
     type = nt[0]
@@ -16,7 +16,7 @@ def reset_modules(input)
   modules.clone.each do |n,m|
     m[:targets].each do |t|
       modules[t] = {type: :output, targets: [], mem: {}} if !modules.key?(t)
-      (modules[t][:mem] ||= {})[n] = :low if modules[t][:type] == :&
+      (modules[t][:mem] ||= {})[n] = false if modules[t][:type] == :&
     end
   end
 
@@ -25,42 +25,42 @@ end
 
 
 def push(modules, part2_mod = nil)
-  pulses = [[:low,:broadcaster,nil]]
-  count = {low: 0, high: 0}
+  res = {false => 0, true => 0}
+  pulses = [[false,:broadcaster,nil]]
 
   until pulses.empty?
     pulse, trgt, src = pulses.shift
     mod = modules[trgt]
 
-    count[pulse] += 1
-    count[:p2_high_inp] = src if trgt == part2_mod && pulse == :high
+    res[pulse] += 1
+    res[:p2_high_inp] = src if trgt == part2_mod && pulse
 
     case mod[:type]
     when :broadcaster
       nxt_pulse = pulse
     when :%
-      next if pulse == :high
-      nxt_pulse = (mod[:on] = !mod[:on]) ? :high : :low
+      next if pulse
+      nxt_pulse = mod[:on] = !mod[:on]
     when :&
       mod[:mem][src] = pulse
-      nxt_pulse = mod[:mem].all?{ _2 == :high } ? :low : :high
+      nxt_pulse = !mod[:mem].all?{ _2 }
     end
     mod[:targets].each{ pulses << [nxt_pulse,_1,trgt] }
   end
 
-  count
+  res
 end
 
 
 start = Time.now
-modules = reset_modules(input)
-part1 = 1000.times.inject([0,0]){ |s,_| c = push(modules); [s[0]+c[:low],s[1]+c[:high]] }.inject(&:*)
+modules = build_modules(input)
+part1 = 1000.times.inject([0,0]){ |s,_| c = push(modules); [s[0]+c[false],s[1]+c[true]] }.inject(&:*)
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 
 start = Time.now
-modules = reset_modules(input)
-part2_mod = modules.keys.detect{ modules[_1][:targets].include?(:rx) }
+modules = build_modules(input)
+part2_mod = modules.detect{ _2[:targets].include?(:rx) }.first
 high_inp = {}
 1.step do |i|
   count = push(modules, part2_mod)
