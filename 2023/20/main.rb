dc67522190efc725083c369e48ae70822b47d356
Chris Brandhorst <chris.brandhorst@gmail.com>
2023-12-20 08:45:24 +0100

    Day 20

---
+++ 2023/20/main.rb
@@ -0,0 +1,73 @@
+start = Time.now
+input = File.readlines("input", chomp: true)
+puts "Prep: #{Time.now - start}s"
+
+def reset_modules(input)
+  modules = input.inject({}) do |mods,l|
+    nt, targets = l.split(" -> ")
+    type = nt[0]
+    mods[type == "b" ? nt.to_sym : nt[1..-1].to_sym] = {
+      type:     type == "b" ? :broadcaster : type.to_sym,
+      targets:  targets.split(", ").map(&:to_sym)
+    }
+    mods
+  end
+
+  modules.clone.each do |n,m|
+    m[:targets].each do |t|
+      modules[t] = {type: :output, targets: [], mem: {}} if !modules.key?(t)
+      (modules[t][:mem] ||= {})[n] = :low if modules[t][:type] == :&
+    end
+  end
+
+  modules
+end
+
+
+def push(modules, part2_mod = nil)
+  pulses = [[:low,:broadcaster,nil]]
+  count = {low: 0, high: 0}
+
+  until pulses.empty?
+    pulse, trgt, src = pulses.shift
+    mod = modules[trgt]
+
+    count[pulse] += 1
+    count[:p2_high_inp] = src if trgt == part2_mod && pulse == :high
+
+    case mod[:type]
+    when :broadcaster
+      nxt_pulse = pulse
+    when :%
+      next if pulse == :high
+      nxt_pulse = (mod[:on] = !mod[:on]) ? :high : :low
+    when :&
+      mod[:mem][src] = pulse
+      nxt_pulse = mod[:mem].all?{ _2 == :high } ? :low : :high
+    end
+    mod[:targets].each{ pulses << [nxt_pulse,_1,trgt] }
+  end
+
+  count
+end
+
+
+start = Time.now
+modules = reset_modules(input)
+part1 = 1000.times.inject([0,0]){ |s,_| c = push(modules); [s[0]+c[:low],s[1]+c[:high]] }.inject(&:*)
+puts "Part 1: #{part1} (#{Time.now - start}s)"
+
+
+start = Time.now
+modules = reset_modules(input)
+part2_mod = modules.keys.detect{ modules[_1][:targets].include?(:rx) }
+high_inp = {}
+1.step do |i|
+  count = push(modules, part2_mod)
+  if src = count[:p2_high_inp]
+    high_inp[src] = i
+    break if high_inp.size == modules[part2_mod][:mem].size
+  end
+end
+part2 = high_inp.values.inject(&:lcm)
+puts "Part 2: #{part2} (#{Time.now - start}s)"
