960b32a8908a1a8de2d67b71cb0ba2ae52577565
Chris Brandhorst <chris.brandhorst@gmail.com>
2023-12-30 10:51:48 +0100

    Day 12: Cleanup

--- 2023/12/main.rb
+++ 2023/12/main.rb
@@ -3,20 +3,17 @@ input = File.readlines("input", chomp: true)
   .map{ r = _1.split; [r.first,r.last.split(",").map(&:to_i)] }
 puts "Prep: #{Time.now - start}s"
 
-def run(row, groups, row_i = 0, group_i = 0, group_len = 0, brk_group_len = 0)
-  c = row[row_i]
-  ret = 0
-
+def run(row, groups, row_i = 0, group_i = 0, group_len = 0, brk_group_len = 0, mem = {})
   mem_key = [
     row[row_i,row.size-row_i],
     groups,
     group_i,
     group_len
   ]
-  if mem_val = @mem[mem_key]
-    return mem_val
-  end
-  
+  return mem[mem_key] if mem.key?(mem_key)
+
+  c, ret = row[row_i], 0
+
   # End of row && Last group is correct length && All groups are present
   if row_i == row.size && brk_group_len == groups.last && groups.size == group_i + 1 - (group_len == 0 ? 1 : 0)
     ret = 1
@@ -25,24 +22,22 @@ def run(row, groups, row_i = 0, group_i = 0, group_len = 0, brk_group_len = 0)
   # Known and possible broken spring && Not more groups then needed && Group is not too long yet
   # Last two statements just make it quit faster
   if (c == "#" || c == "?") && group_i < groups.size && group_len < groups[group_i]
-    ret += run(row, groups, row_i + 1, group_i, group_len + 1, group_len + 1)
+    ret += run(row, groups, row_i + 1, group_i, group_len + 1, group_len + 1, mem)
   end
 
   # Known and possible operational spring && (Current group is empty || Current is finished)
   if (c == "." || c == "?") && (group_len == 0 || group_len == groups[group_i])
-    ret += run(row, groups, row_i + 1, group_i + (group_len == 0 ? 0 : 1), 0, brk_group_len)
+    ret += run(row, groups, row_i + 1, group_i + (group_len == 0 ? 0 : 1), 0, brk_group_len, mem)
   end
 
-  @mem[mem_key] = ret
+  mem[mem_key] = ret
 end
 
 
 start = Time.now
-@mem = {}
 part1 = input.sum{ |s,g| run(s.chars, g) }
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 start = Time.now
-@mem = {}
 part2 = input.sum{ |s,g| run( ([s]*5).join("?").chars , g*5 ) }
 puts "Part 2: #{part2} (#{Time.now - start}s)"
--- 2023/21/main.rb
+++ 2023/21/main.rb
@@ -9,26 +9,26 @@ puts "Prep: #{Time.now - start}s"
 
 
 def walk(gardens, start, steps)
-  dadj = [[0,-1],[1,0],[0,1],[-1,0]]
   poss = Set.new([start])
+  res = [nil]
 
   steps.times do |i|
     poss = poss.inject(Set.new) do |ps,c|
-      dadj.each do |dx,dy|
-        ac = [c[0]+dx,c[1]+dy]
+      gardens.each_adj(c) do |ac,v|
         ps << ac if gardens[ac] != "#"
       end
       ps
     end
+    res << poss.size
   end
 
-  poss
+  res
 end
 
 
 start = Time.now
 spos = gardens.detect{ _2 == "S" }
-part1 = walk(gardens, spos, 64).size
+part1 = walk(gardens, spos, 64).last
 puts "Part 1: #{part1} (#{Time.now - start}s)"
 
 
@@ -62,13 +62,14 @@ STEPS = 26501365
 size = gardens.row_count
 sizec = size / 2
 
-ys = []
-ys << walk(gardens, spos, sizec).size
-puts "Got y0: #{ys.last}..."
-ys << walk(gardens, spos, sizec + size).size
-puts "Got y1: #{ys.last}..."
-ys << walk(gardens, spos, sizec + size * 2).size
-puts "Got y2: #{ys.last}..."
+# ys = []
+# ys << walk(gardens, spos, sizec).size
+# puts "Got y0: #{ys.last}..."
+# ys << walk(gardens, spos, sizec + size).size
+# puts "Got y1: #{ys.last}..."
+# ys << walk(gardens, spos, sizec + size * 2).size
+# puts "Got y2: #{ys.last}..."
+ys = [3868,34368,95262]
 
 # Define a, b and c
 a = (ys[0] - 2*ys[1] + ys[2]) / 2;
@@ -80,3 +81,101 @@ puts "Formula is: #{a}x² + #{b}x + #{c}"
 x = STEPS / size
 part2 = a*x**2 + b*x + c
 puts "Part 2: #{part2} (#{Time.now - start}s)"
+
+
+
+
+
+
+
+# At step 65
+#            
+#            
+#            
+#     /\     
+#     \/     
+#            
+#            
+#            
+# 3868
+
+# At step 65 + 131 = 196
+#     /\     
+#   / OO \   
+#            
+#  /O OO O\  
+#  \O OO O/  
+#            
+#   \ OO /   
+#     \/     
+# 7584 + 2x(991+1005) + 2x(5719+5732) = 34478 (110 diff)
+
+# At step 65 + 131 + 131 = 327
+#        /\        
+#      / OO \      
+#                  
+#     /O OO O\     
+#   / OO OO OO \   
+#                  
+#  /O OO OO OO O\  
+#  \O OO OO OO O/  
+#                  
+#   \ OO OO OO /   
+#     \O OO O/     
+#                  
+#      \ OO /      
+#        \/        
+# 3x7584 + 2x7613 + 4x(991+1005) + 2x(6700+6701) + 2x(5719+5732) = 95666 (404 diff)
+
+
+# Rhombus:    3868
+# Filled:     7584 / 7613
+# Diag small: 991 / 1005
+# Diag large: 6700 / 6701
+# Corner:     5719 / 5732
+
+center = walk(gardens, spos, sizec + 2 * size)
+corner_t = walk(gardens, [sizec,size-1], sizec + 2 * size)
+corner_r = walk(gardens, [0,sizec], sizec + 2 * size)
+corner_b = walk(gardens, [sizec,0], sizec + 2 * size)
+corner_l = walk(gardens, [size-1,sizec], sizec + 2 * size)
+
+center[sizec]
+center[sizec + size]
+center[sizec + 2 * size]
+# 3868
+# 7613
+# 7584
+# Further: 7613, 7584 etc
+
+
+# TODO:
+# - Walk for each of the possible tile states:
+#   - Rhombus
+#   - Filled
+#   - Corners, T/R/B/L
+#   - Small diag TR/BR/BL/TL
+#   - Large diag TR/BR/BL/TL
+# - Calculate how many of each type we have at the target step
+# - Multiply
+
+
+
+# # spos = [0,sizec]
+# poss = Set.new([spos])
+# 0.step do |i|
+#   p gardens
+#   puts "Step #{i}: #{poss.size} positions"
+#   gets
+#   poss = poss.inject(Set.new) do |ps,c|
+#     gardens[c] = "."
+#     gardens.each_adj(c) do |ac,v|
+#       if v != "#"
+#         ps << ac
+#         gardens[ac] = "O"
+#       end
+#     end
+#     ps
+#   end
+# end
+
