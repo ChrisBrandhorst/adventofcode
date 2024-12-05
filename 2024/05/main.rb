e19974efe92cc9805609bf214ee42063be271409
Chris Brandhorst <chris.brandhorst@gmail.com>
2024-12-05 12:04:00 +0100

    Day 5: Mand!

--- 2024/05/main.rb
+++ 2024/05/main.rb
@@ -5,26 +5,22 @@ def prep
     .split("\n\n")
     .map{ |b| b.split("\n").map{ _1.split(/[|,]/).map(&:to_i) } }
 
-  correct, corrected = [], []
+  correct, incorrect = updates.partition{ |u| u.each_cons(2).all?{ rules.include?(_1) } }
+  [rules, correct, incorrect]
+end
 
+def part2(rules, updates)
   updates.each do |u|
-    if u.each_cons(2).all?{ rules.include?(_1) }
-      correct << u
-    else
-      u_rules = rules.select{ (_1 & u).size == 2 }
-      corrected << u.sort do |a,b|
-        u_rules.detect{ _1.include?(a) && _1.include?(b) }.first == a ? -1 : 1
-      end
-    end
+    u_rules = rules.select{ (_1 & u).size == 2 }
+    u.sort!{ |a,b| u_rules.detect{ _1.include?(a) && _1.include?(b) }.first == a ? -1 : 1 }
   end
-
-  [correct, corrected]
+  get_middle_sum(updates)
 end
 
 def get_middle_sum(updates)
   updates.sum{ _1[_1.size/2] }
 end
 
-correct, corrected = time("Prep", false){ prep }
+rules, correct, incorrect = time("Prep", false){ prep }
 time("Part 1"){ get_middle_sum(correct) }
-time("Part 2"){ get_middle_sum(corrected) }
+time("Part 2"){ part2(rules, incorrect) }
